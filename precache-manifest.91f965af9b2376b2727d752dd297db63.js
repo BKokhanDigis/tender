6586b7306ac9fe803bc82479a71b3e0de0200d2e
Bogdan Kokhan <b.kokhan@digiscorp.com>
2023-09-21 12:56:24 +0300

    Updates

---
+++ precache-manifest.91f965af9b2376b2727d752dd297db63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "83e4046d1b951f9186f1",
+    "url": "/tender/static/css/main.a8b1b892.chunk.css"
+  },
+  {
+    "revision": "83e4046d1b951f9186f1",
+    "url": "/tender/static/js/main.d1b86579.chunk.js"
+  },
+  {
+    "revision": "b9ffaa4f6b6e00b364f8",
+    "url": "/tender/static/js/runtime~main.fc12d764.js"
+  },
+  {
+    "revision": "6b2a940b807836f7af0d",
+    "url": "/tender/static/js/2.1a376969.chunk.js"
+  },
+  {
+    "revision": "c45745a675d03f61316d46c4ea41c9d6",
+    "url": "/tender/static/media/Add.c45745a6.svg"
+  },
+  {
+    "revision": "6b06ac5d5b56acbf1e466524cb3802fc",
+    "url": "/tender/static/media/Add_disabled.6b06ac5d.svg"
+  },
+  {
+    "revision": "d2bcce3b157a4cb6746eeb473b69d6bd",
+    "url": "/tender/index.html"
+  }
+];
